b98073c26ae2bcfa64cd5bb267eedfefdacfc01c
Joshua Sullivan <Joshua Sullivan>
2016-12-05 14:13:07 -0800

    Errors.json() function + err.isGeneric() function

--- src/errors.js
+++ src/errors.js
@@ -21,6 +21,7 @@ function exportFn(){
     SuperErrors.extend = extendError;
     SuperErrors.stack = getCustomStack;
     SuperErrors.rebase = rebaseError;
+    SuperErrors.json = errorToJSON;
     
     return SuperErrors;
 }
@@ -142,6 +143,7 @@ function extendError(constructor, name, default_message, status_code, client_saf
     constructor.prototype.message = default_message;
     constructor.prototype.client_safe_message = default_message;
     constructor.prototype.status_code = (status_code && typeof status_code === 'number' ? status_code : 500);
+    constructor.prototype.isGeneric = setGeneric;
     
     constructor.base = this;
     constructor.client_safe_messages = (client_safe_messages ? true : false);
@@ -167,6 +169,14 @@ function getCustomStack(err, include_sub_errors){
     
     if(err && (err.from || err.additional || (include_sub_errors !== false && (err.errors || err.fields)))){
         stack += '\n    ---';
+        
+        if(err.additional){
+            try {
+                stack += getSubStack('additional info', JSON.stringify(err.additional, null, '    '));
+            } catch(err){
+                stack += getSubStack('additional info error', err.message);
+            }
+        }
     
         if(err.from){
             stack += getSubStack('from', SuperErrors.stack(err.from, false));
@@ -179,18 +189,10 @@ function getCustomStack(err, include_sub_errors){
                 }
             }
             
-            if(err.fields && typeof err.fields === 'object'){
+            if(include_sub_errors !== 'addl' && err.fields && typeof err.fields === 'object'){
                 for(var field in err.fields){
-                    stack += getSubStack(field, SuperErrors.stack(err.fields[field], false));
+                    stack += getSubStack(field, SuperErrors.stack(err.fields[field], 'addl'));
                 }
-            }
-        }
-        
-        if(err.additional){
-            try {
-                stack += getSubStack('additional info', JSON.stringify(err.additional, null, '    '));
-            } catch(err){
-                stack += getSubStack('additional info error', err.message);
             }
         }
     }
@@ -351,4 +353,158 @@ function rebaseError(base, err){
     }
     
     return err;
+}
+
+function errorToJSON(err, map, exclude){
+    var json = {};
+    var mapped, i, a, submap, subfield;
+    
+    if(!exclude || typeof exclude !== 'object'){
+        exclude = {};
+    }
+    
+    if(map === 'all'){
+        map = {
+            message: 'message',
+            client_safe_message: 'client_safe_message',
+            errors: 'errors',
+            field: 'field',
+            fields: 'fields',
+            from: 'from',
+            name: 'name',
+            stack: 'stack',
+            status_code: 'status_code'
+        };
+    }
+    
+    else if(!map || typeof map !== 'object'){
+        map = {
+            "client_safe_message": 'message',
+            "errors.client_safe_message": 'errors',
+            "field": 'field',
+            "fields.client_safe_message": 'fields',
+            "name": 'name',
+            "status_code": 'status_code'
+        };
+    }
+    
+    if(Array.isArray(err)){
+        // grab the first and convert the others to additional errors
+        a = err.slice(1);
+        err = err[0];
+        if(err && typeof err === 'object'){
+            if(Array.isArray(err)){
+                err = { name:'UnknownError', message: '[array of arrays]' };
+            }
+            for(i = 0; i < a.length; i++){
+                err = addError(err, a[i]);
+            }
+        }
+    }
+    
+    if(!err || typeof err !== 'object'){
+        if(typeof err === 'function'){
+            err = '[function]';
+        }
+        err = { 
+            type: 'UnknownError',
+            message: err
+        };
+    }
+    
+    for(var field in map){
+        mapped = map[field];
+        i = field.indexOf('.');
+        
+        if(~i){
+            subfield = field.substr(i + 1);
+            field = field.substr(0, i);
+        } else {
+            subfield = undefined;
+        }
+        
+        if(field in exclude && exclude[field] === true){
+            continue;
+        }
+        
+        if(field === 'stack'){
+            json[mapped] = getErrorStack(err);
+            continue;
+        }
+        
+        if(field in err){
+            switch(field){
+                case 'from':
+                    if(subfield){
+                        submap = {};
+                        submap[subfield] = subfield;
+                        json[mapped] = errorToJSON(err.from, submap)[subfield];    
+                    } else {
+                        json[mapped] = errorToJSON(err.from, map, merge(exclude, ('from' in exclude ? exclude.from : { fields:true, errors:true, status_code:true })));
+                    }
+                    break;
+                case 'errors':
+                    if(subfield){
+                        submap = {};
+                        submap[subfield] = subfield; 
+                    }
+                    if(err.errors.length > 0){
+                        a = [];
+                        for(i = 0; i < err.errors.length; i++){
+                            if(subfield){
+                                a[i] = errorToJSON(err.errors[i], submap)[subfield];  
+                            } else {
+                                a[i] = errorToJSON(err.errors[i], map, merge(exclude, ('errors' in exclude ? exclude.errors : { fields:true, errors:true, status_code:true })));
+                            }
+                        }
+                        json[mapped] = a;
+                    }
+                    break;
+                case 'fields':
+                    if(subfield){
+                        submap = {};
+                        submap[subfield] = subfield;
+                    }
+                    a = {};
+                    for(i in err.fields){
+                        if(subfield){
+                            a[i] = errorToJSON(err.fields[i], submap)[subfield];  
+                        } else {
+                            a[i] = errorToJSON(err.fields[i], map, merge(exclude, ('fields' in exclude ? exclude.fields : { fields:true, status_code:true })));
+                        }
+                    }
+                    json[mapped] = a;
+                    break;
+                default:
+                    json[mapped] = err[field];
+            }
+        } else {
+            switch(field){
+                case 'client_safe_message':
+                case 'message':
+                    json[mapped] = 'There was an error.';
+                    break;
+                case 'name':
+                    json[mapped] = 'UnknownError';
+                    break;
+                case 'status_code':
+                    json[mapped] = 500;
+                    break;
+            }
+        }
+    }
+    return json;
+}
+
+function merge(a, b){
+    var r = {}, f;
+    for(f in a) r[f] = a[f];
+    for(f in b) r[f] = b[f];
+    return r;
+}
+
+function setGeneric(value){
+    /* jshint validthis:true */
+    this.generic = (value !== false);
+    return this;
 }
--- test/test-errors.js
+++ test/test-errors.js
@@ -440,6 +440,217 @@ describe('Errors', function(){
         });
     });
     
+    describe('generic()', function(){
+        it('should exist on a super error instance', function(){
+            var Errors = require('../src/errors.js')();
+            require('../src/inherits.js')(Errors);
+            
+            function TestError(){
+                return TestError.init(this, arguments);
+            }
+            Errors.extend(TestError, 'TestError', 'Test error.', 500, true);
+            
+            var t = Errors.TestError('test');
+            expect(t.isGeneric).to.be.a('function');
+            expect(t.isGeneric()).to.equal(t);
+            expect(t.generic).to.equal(true);
+            expect(t.isGeneric(false)).to.equal(t);
+            expect(t.generic).to.equal(false);
+        });
+    });
+    
+    describe('json()', function(){
+        it('should return a client-safe JSON serializable object by default', function(){
+            expect(Errors.json({ message:'hi' })).to.deep.equal({ message:'There was an error.', name:'UnknownError', status_code:500 });
+            expect(Errors.json({ name:'NotifyUser', client_safe_message:'Bad stuff happened...', stack:'unsafe...', status_code:400 })).to.deep.equal({ name:'NotifyUser', message:'Bad stuff happened...', status_code:400 });
+        });
+        
+        it('should handle primative types', function(){
+            var default_json = { message:'There was an error.', name:'UnknownError', status_code:500 };
+            expect(Errors.json(true)).to.deep.equal(default_json);
+            expect(Errors.json(false)).to.deep.equal(default_json);
+            expect(Errors.json('')).to.deep.equal(default_json);
+            expect(Errors.json({})).to.deep.equal(default_json);
+            expect(Errors.json([])).to.deep.equal(default_json);
+            expect(Errors.json([[]])).to.deep.equal(default_json);
+            expect(Errors.json([{}])).to.deep.equal(default_json);
+            expect(Errors.json([{}, {}])).to.deep.equal({
+                errors:["There was an error."],
+                message:'There was an error.',
+                name:'UnknownError',
+                status_code:500
+            });
+            expect(Errors.json(function(){ return 'hi'; })).to.deep.equal(default_json);
+        });
+        
+        it('should handle all the values', function(){
+            var Errors = require('../src/errors.js')();
+            require('../src/inherits.js')(Errors);
+            
+            function TestError(){
+                return TestError.init(this, arguments);
+            }
+            Errors.extend(TestError, 'TestError', 'Test error.', 500, true);
+            
+            var addl = Errors.TestError('additional error', 'additional additional', Errors.TestError('from additional'));
+            Errors.add(addl, Errors.TestError('additional additional error'));
+            Errors.add(addl, 'field', Errors.TestError('additional field error'));
+            
+            var field = Errors.TestError('field error', 'field additional', Errors.TestError('from field'));
+            Errors.add(field, Errors.TestError('field additional error'));
+            Errors.add(field, 'field', Errors.TestError('field field error'));
+            
+            var err = Errors.TestError('test error', 'additional info', Errors.TestError('from'));
+            Errors.add(err, addl);
+            Errors.add(err, 'field', field);
+            
+            expect(Errors.json(err)).to.deep.equal({
+                errors: [
+                    "additional error",
+                    "additional additional error"
+                ],
+                fields: {
+                    field: "additional field error",
+                    "field.field": "field field error"
+                },
+                message: "test error",
+                name: "TestError",
+                status_code: 500
+            });
+        });
+        
+        it('should be able to map all', function(){
+            var Errors = require('../src/errors.js')();
+            require('../src/inherits.js')(Errors);
+            
+            function TestError(){
+                return TestError.init(this, arguments);
+            }
+            Errors.extend(TestError, 'TestError', 'Test error.', 500, true);
+            
+            var addl = Errors.TestError('additional error', 'additional additional', Errors.TestError('from additional'));
+            Errors.add(addl, Errors.TestError('additional additional error'));
+            Errors.add(addl, 'field', Errors.TestError('additional field error'));
+            
+            var field = Errors.TestError('field error', 'field additional', Errors.TestError('from field'));
+            Errors.add(field, Errors.TestError('field additional error'));
+            Errors.add(field, 'field', Errors.TestError('field field error'));
+            
+            var err = Errors.TestError('test error', 'additional info', Errors.TestError('from'));
+            Errors.add(err, addl);
+            Errors.add(err, 'field', field);
+            
+            expect(Errors.json(err, 'all')).to.deep.equal({
+                client_safe_message: "test error",
+                errors: [
+                    {
+                        client_safe_message: "additional error",
+                        from: {
+                            client_safe_message: "from additional",
+                            message: "from additional",
+                            name: "TestError",
+                            stack: "TestError: from additional"
+                        },
+                        message: "additional error",
+                        name: "TestError",
+                        stack: "TestError: additional error"
+                    },
+                    {
+                        client_safe_message: "additional additional error",
+                        message: "additional additional error",
+                        name: "TestError",
+                        stack: "TestError: additional additional error"
+                    }
+                ],
+                fields: {
+                    field: {
+                        client_safe_message: "additional field error",
+                        errors: [
+                            {
+                                client_safe_message: "field error",
+                                from: {
+                                    client_safe_message: "from field",
+                                    message: "from field",
+                                    name: "TestError",
+                                    stack: "TestError: from field"
+                                },
+                                message: "field error",
+                                name: "TestError",
+                                stack: "TestError: field error"
+                            },
+                            {
+                                client_safe_message: "field additional error",
+                                message: "field additional error",
+                                name: "TestError",
+                                stack: "TestError: field additional error"
+                            }
+                        ],
+                        message: "additional field error",
+                        name: "TestError",
+                        stack: "TestError: additional field error"
+                    },
+                    "field.field": {
+                        client_safe_message: "field field error",
+                        message: "field field error",
+                        name: "TestError",
+                        stack: "TestError: field field error"
+                    }
+                },
+                from: {
+                    client_safe_message: "from",
+                    message: "from",
+                    name: "TestError",
+                    stack: "TestError: from"
+                },
+                message: "test error",
+                name: "TestError",
+                stack: "TestError: test error",
+                status_code: 500
+            });
+        });
+        
+        it('should not include errors if errors is an empty array', function(){
+            expect(Errors.json({ errors:[] })).to.deep.equal({
+                message: "There was an error.",
+                name: "UnknownError",
+                status_code: 500
+            });
+        });
+        
+        it('should work with a from subfield map', function(){
+            expect(Errors.json({ from:{ message:'test' } }, { "from.message":'error_from' })).to.deep.equal({
+                error_from: 'test'
+            });
+        });
+        
+        it('should allow deep exclusion definitions', function(){
+            expect(Errors.json(
+                {
+                    from:{ message:'test' },
+                    errors: [{ message:'test2' }],
+                    fields: { test: { message:'test3' } }
+                },
+                'all',
+                {
+                    client_safe_message:true,
+                    from: { status_code:true, name:true, stack:true },
+                    errors: { status_code:true, name:true, stack:true },
+                    fields: { status_code:true, name:true, stack:true },
+                    stack:true,
+                    status_code:true
+                }
+            )).to.deep.equal({
+                from: { message:'test' },
+                errors: [{ message:'test2' }],
+                fields: {
+                    test: { message:'test3' }
+                },
+                message: 'There was an error.',
+                name: 'UnknownError'
+            });
+        });
+    });
+    
     describe('rebase()', function(){
         it('should exist', function(){
             expect(Errors.rebase).to.be.a('function');
@@ -715,7 +926,7 @@ describe('Errors', function(){
             Errors.add(err, addl);
             Errors.add(err, 'field', field);
             
-            expect(Errors.stack(err)).to.equal("TestError: test error\n    ---\n    from: TestError: from\n    additional error: TestError: additional error\n        ---\n        from: TestError: from additional\n        additional info: \"additional additional\"\n    additional error: TestError: additional additional error\n    field: TestError: additional field error\n    field.field: TestError: field field error\n    additional info: \"additional info\"");
+            expect(Errors.stack(err)).to.equal("TestError: test error\n    ---\n    additional info: \"additional info\"\n    from: TestError: from\n    additional error: TestError: additional error\n        ---\n        additional info: \"additional additional\"\n        from: TestError: from additional\n    additional error: TestError: additional additional error\n    field: TestError: additional field error\n        ---\n        additional error: TestError: field error\n            ---\n            additional info: \"field additional\"\n            from: TestError: from field\n        additional error: TestError: field additional error\n    field.field: TestError: field field error");
         });
         
         it('should match the result of the inst.stack getter', function(){
@@ -741,8 +952,6 @@ describe('Errors', function(){
             
             Errors.add(err, 'field', field);
             Errors.add(err, addl);
-            
-            console.log(err.stack);
             
             expect(Errors.stack(err)).to.equal(err.stack);
         });
